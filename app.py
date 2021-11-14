60ac0517db2cb526547a603d1b1e74a6ccfab94a
Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>
2021-11-14 08:46:27 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from sqlalchemy import true
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+
+
+
+
