32cba2ffb2b9539c339e863e5ac4c41d90e88aad
Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>
2021-11-14 13:24:51 +0100

    Added routes for signup, login and logout.
    Signed-off-by: Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,15 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/logginn')
+def logginn():
+    return "<p>Logget inn<p>"
+
+@auth.route('/loggut')
+def loggut():
+    return "<p>Logget ut<p>"
+
+@auth.route('/registrer_deg')
+def registrer_deg():
+    return "<p>Registrer deg<p>"
+
