4fe3858c3a14e52527bf54591e5680218a741f66
Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>
2021-11-14 09:33:15 +0100

    Added database (MongoDB)
    Signed-off-by: Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from sqlalchemy import true
-
 from website import create_app
 
 app = create_app()
@@ -7,9 +5,9 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+#@app.route('/')
+#def hello_world():  # put application's code here
+#    return 'Hello World!'
 
 
 
