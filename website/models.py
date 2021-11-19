02ca9718189bff4b92d41e81d43f574d0f142ffc
Ronny Wilhelmsen <31963657+ronnywilhelmsen@users.noreply.github.com>
2021-11-19 08:17:08 +0100

    Lagt til Logginn i models.py for å logge alle innloggingen til alle brukerne.
    Signed-off-by: Ronny Wilhelmsen <ronny.wilhelmsen@gmail.com>

--- website/models.py
+++ website/models.py
@@ -1,7 +1,13 @@
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 from website import db
 
+
+class Logginn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,5 +16,5 @@ class User(db.Model, UserMixin):
     fornavn = db.Column(db.String(100))
     etternavn = db.Column(db.String(150))
     rolle = db.Column(db.String(150))
-    datetime = db.Column(db.DateTime(timezone=True))
-
+    datetime = db.Column(db.DateTime(timezone=True), default=func.now())
+    innlogginger = db.relationship('Logginn')
